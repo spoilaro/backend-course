a5350d8b5dd97c9ec06babac0865a224495bfe42
Johannes <johannes.haapanen@gmail.com>
2021-05-25 19:33:25 +0300

    Made Os module excercise

--- coursework/node_module/references/os_demo.js
+++ coursework/node_module/references/os_demo.js
@@ -1,1 +1,23 @@
 const os = require("os");
+
+//Platform
+//console.log(os.platform());
+
+//CPU Arch
+//console.log(os.arch());
+
+//CPU Cores
+//console.log(os.cpus());
+
+//Memory
+//console.log(os.freemem());
+
+//Total Mem
+//console.log(os.totalmem());
+
+//Home dir
+//console.log(os.homedir());
+
+//Uptime
+//console.log(os.uptime());
+
