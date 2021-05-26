035e8dac272bb2aec7e8114ec965b1d56017eb1a
Johannes <johannes.haapanen@gmail.com>
2021-05-26 08:10:14 +0300

    Did the event module

---
+++ coursework/node_module/references/http_demo.js
@@ -0,0 +1,9 @@
+//const http = require("http");
+//
+////Create obj
+//http.createServer((req, res) => {
+//    res.write("Hello me");
+//    res.end();
+//}).listen(5005, () => {
+//    console.log("Service is running!");
+//})
