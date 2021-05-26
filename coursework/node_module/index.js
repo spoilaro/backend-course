e838183e9280a7ccff1de198c13883bf634091e8
Johannes <johannes.haapanen@gmail.com>
2021-05-26 13:48:52 +0300

    Made server to serve html files

--- coursework/node_module/index.js
+++ coursework/node_module/index.js
@@ -2,14 +2,21 @@ const http = require("http");
 const path = require("path");
 const fs = require("fs");
 
-const server = http.createServer((req, res) =>  {
-    if(req.url === "/") {
-        res.end("<h1>Home</h1>");
-    };
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    fs.readFile(
+      path.join(__dirname, "public", "index.html"),
+      (err, content) => {
+        err ? console.log(err) : null;
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(content);
+      }
+    );
+  }
 });
 
 const PORT = process.env.PORT || 5005;
 
 server.listen(PORT, () => {
-    console.log("Server is running on port: " + PORT.toString());
-})
+  console.log("Server is running on port: " + PORT.toString());
+});
