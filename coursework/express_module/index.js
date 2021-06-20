3e889b7cf2ded2d3bda683e4abc311023e1bf452
Johannes <johannes.haapanen@gmail.com>
2021-06-20 06:17:02 +0300

    Added custom middleware

--- coursework/express_module/index.js
+++ coursework/express_module/index.js
@@ -1,11 +1,25 @@
 const express = require("express");
 const path = require("path");
+const members = require("./members");
 
 const app = express();
+
+const logger = (req, res, next) => {
+  console.log("Hello");
+  next();
+};
+
+app.use(logger);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
+
+app.get("/api/members", (req, res) => {
+  res.json(members);
+});
+
+app.use(express.static(path.join(__dirname, "public")));
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
