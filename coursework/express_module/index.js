4d2f8e628c8c6b86b61db7918e37228e05026bef
Johannes <johannes.haapanen@gmail.com>
2021-06-20 09:58:15 +0300

    Added basic status change

--- coursework/express_module/index.js
+++ coursework/express_module/index.js
@@ -1,15 +1,12 @@
+//Imports
 const express = require("express");
 const path = require("path");
 const members = require("./members");
+const logger = require("./middleware/logger");
 
 const app = express();
 
-const logger = (req, res, next) => {
-  console.log("Hello");
-  next();
-};
-
-app.use(logger);
+//app.use(logger);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
@@ -17,6 +14,17 @@ app.get("/", (req, res) => {
 
 app.get("/api/members", (req, res) => {
   res.json(members);
+});
+
+app.get("/api/members/:id", (req, res) => {
+  var id = req.params.id;
+  const found = members.some((mem) => mem.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter((mem) => mem.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: "Not found" });
+  }
 });
 
 app.use(express.static(path.join(__dirname, "public")));
