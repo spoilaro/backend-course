d492334ff75e5e1afab7ba4b0f53182a5df94a95
Johannes <johannes.haapanen@gmail.com>
2021-06-20 11:02:31 +0300

    Added route to update members list

--- coursework/express_module/index.js
+++ coursework/express_module/index.js
@@ -1,10 +1,16 @@
 //Imports
 const express = require("express");
 const path = require("path");
-const members = require("./members");
+const members = require("./Members");
 const logger = require("./middleware/logger");
 
+//ROutes
+const memberRoute = require("./routes/api/membersRoute");
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 //app.use(logger);
 
@@ -12,22 +18,9 @@ app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get("/api/members", (req, res) => {
-  res.json(members);
-});
-
-app.get("/api/members/:id", (req, res) => {
-  var id = req.params.id;
-  const found = members.some((mem) => mem.id === parseInt(req.params.id));
-
-  if (found) {
-    res.json(members.filter((mem) => mem.id === parseInt(req.params.id)));
-  } else {
-    res.status(400).json({ msg: "Not found" });
-  }
-});
-
 app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/api/members", memberRoute);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
---
+++ coursework/express_module/routes/api/membersRoute.js
@@ -0,0 +1,56 @@
+const express = require("express");
+const router = express.Router();
+const members = require("../../Members");
+const uuid = require("uuid");
+
+router.get("/", (req, res) => {
+  res.json(members);
+});
+
+router.get("/:id", (req, res) => {
+  var id = req.params.id;
+  const found = members.some((mem) => mem.id === parseInt(req.params.id));
+
+  if (found) {
+    res.json(members.filter((mem) => mem.id === parseInt(req.params.id)));
+  } else {
+    res.status(400).json({ msg: "Not found with id of " + req.params.id });
+  }
+});
+
+router.post("/", (req, res) => {
+  const newMember = {
+    id: uuid.v4(),
+    name: req.body.name,
+    email: req.body.email,
+    status: "active",
+  };
+
+  if (!newMember.name || !newMember.email) {
+    return res.status(400).json({ msg: "Name and email not given" });
+  }
+
+  members.push(newMember);
+  res.json(members);
+});
+
+router.put("/:id", (req, res) => {
+  var id = req.params.id;
+  const found = members.some((mem) => mem.id === parseInt(req.params.id));
+
+  if (found) {
+    const updateMember = req.body;
+    members.forEach((mem) => {
+      if (mem.id === parseInt(req.params.id)) {
+        mem.name = updateMember.name ? updateMember.name : mem.name;
+        mem.email = updateMember.email ? updateMember.email : mem.email;
+
+        res.json({ msg: "Member was updated", mem });
+      }
+    });
+  } else {
+    res.status(400).json({ msg: "Not found with id of " + req.params.id });
+  }
+});
+
+module.exports = router;
